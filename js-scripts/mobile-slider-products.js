ea71347caba8a0b40d9f3e5eaeb55ed2563607a8
Adrian Kaczmarek <adrian.kaczmarek@web-solutions.eu>
2023-06-18 20:23:22 +0200

    mobile phone design completed

--- js-scripts/mobile-slider-products.js
+++ js-scripts/mobile-slider-products.js
@@ -1,7 +1,40 @@
 $(".product__products").slick({
   dots: false,
-  infinite: true,
+  infinite: false,
+  arrows: false,
   speed: 300,
-  slidesToShow: 2,
-  adaptiveHeight: true
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
